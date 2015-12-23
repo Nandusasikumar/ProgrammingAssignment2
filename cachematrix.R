6e7c3604b63f7d5d1c71a6d46621dc45723632b4
Nandu Sasikumar <nandug@yahoo.com>
2015-12-23 11:57:19 -0500

    Second commit with function changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Code base for the assignment 2 for the Coursera R Programming course.
+## We are required to write 2 functions which shows the power of R to cache info so it can be looked up again and again
+## Even though i am an engineer by profession, it is been a while since i have dealt with matrix
+## Thought of adding this URL which would help to understand how/why we inverse a function
+## https://www.khanacademy.org/math/linear-algebra/matrix_transformations/inverse_of_matrices/v/linear-algebra-example-of-finding-matrix-inverse
 
-## Write a short comment describing this function
+## First function creates a special "matrix" object that can cache its inverse.  
 
 makeCacheMatrix <- function(x = matrix()) {
+ x + 1
+}  
 
-}
 
-
-## Write a short comment describing this function
+## Second function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve 
+## should retrieve the inverse from the cache.  
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  x +2
+}  
